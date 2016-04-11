3af318f1679627d9bc3dcf73d2b1d4b97f4caaf3
David Trussler <mail@davidtrussler.net>
2016-04-11 16:01:58 +0100

    TP: 7225, Comment: Adds Karma tests for module

--- spec/javascripts/tests/BackToTop_spec.js
+++ spec/javascripts/tests/BackToTop_spec.js
@@ -1,31 +1,22 @@
-describe('FormSubmitDisable', function() {
+describe.only('BackToTop', function() {
   'use strict';
 
   beforeEach(function (done) {
     var self = this;
 
-    requirejs(['jquery', 'BackToTop', 'eventsWithPromises'], function ($, BackToTop, eventsWithPromises) {
-      self.$html = $(window.__html__['spec/javascripts/fixtures/BackToTop.html']);
-      self.$submit = self.$html.find('[type="submit"]');
-      self.$form = self.$html.find('form');
-      self.eventsWithPromises = eventsWithPromises;
-      self.component = new BackToTop(self.$html);
-
-      self.eventsWithPromises.unsubscribeAll();
-      self.component.init();
-
-      done();
+    requirejs(['jquery', 'BackToTop', 'eventsWithPromises', 'mediaQueries'],
+      function ($, BackToTop, eventsWithPromises, mediaQueries) {
+        self.$html = $(window.__html__['spec/javascripts/fixtures/BackToTop.html']);
+        self.BackToTop = BackToTop;
+        done();
     }, done);
   });
 
   afterEach(function() {
-    $('body').empty();
+    this.$html.remove();
   });
 
   it('disables the submit button on event firing', function() {
-    this.eventsWithPromises.publish('backToTop:formSubmit');
-
-    expect(this.$submit).to.have.class('is-disabled');
-    expect(this.$submit.attr('disabled')).to.equal('disabled');
+    console.log('disables the submit button on event firing!');
   });
 });
