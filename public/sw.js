58ccbed6f719d1ad170c8382cbfcf2d24832fbe1
David Trussler <mail@davidtrussler.net>
2016-09-13 09:27:36 +0100

    Comment: SW hijacks the request

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,11 @@
 // Interact with the registered Service Worker via its API
 
 this.addEventListener('fetch', function(event) {
-	console.log(event.request);
+	// console.log(event.request);
+  event.respondWith(
+    new Response(
+      '<h1>helloooo <b>the</b> <i>world!</i></h1>', {
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });
-
