be982b62f5704ef3cbd53f9316025f85931fbf1c
David Trussler <mail@davidtrussler.net>
2016-09-21 16:52:36 +0100

    Comment: Saves assets in cache

--- public/sw.js
+++ public/sw.js
@@ -2,32 +2,17 @@
 
 // caching
 this.addEventListener('install', function(event) {
-  console.log(event);
+  var urlsToCache = [
+        '/',
+        '/assets/application.js',
+        '/assets/enhanced_responsive.css'
+      ];
 
-  event.waitUntil()
-});
-
-/*
-this.addEventListener('fetch', function(event) {
-	// console.log(event.request);
-  event.respondWith(
-    // test for network and send appropriate response
-    fetch(event.request).then(function(response) {
-      console.log(response);
-
-      if (response.status == 404) {
-        return new Response(
-          '<h1 style="font-weight: normal;">The <i>MAS</i> site is <b>not</b> available at this URL (aka a 404 error)!</h1>', {
-          headers: {'Content-Type': 'text/html'}
-        })
-      }
-      return response;
-    }).catch(function() {
-      return new Response(
-        '<h1 style="font-weight: normal;">The <i>MAS</i> site has <b>failed</b> to load \'cos of no network!</h1>', {
-        headers: {'Content-Type': 'text/html'}
-      });
+  event.waitUntil(
+    // open a cache named 'mas-static-v4'
+    caches.open('mas-static-v4').then(function(cache) {
+      // add the urls in urlsToCache to opened cache
+      return cache.addAll(urlsToCache);
     })
   );
 });
-*/
