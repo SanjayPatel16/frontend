aeced9e5a3c9e90af2798dfca7b53b6bd7cfe914
David Trussler <mail@davidtrussler.net>
2016-09-28 17:00:03 +0100

    Comment: retrieves cached assets

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Interact with the registered Service Worker via its API
 
-// caching
+/* cache assets
 this.addEventListener('install', function(event) {
   var urlsToCache = [
         '/',
@@ -10,9 +10,22 @@ this.addEventListener('install', function(event) {
 
   event.waitUntil(
     // open a cache named 'mas-static-v4'
-    caches.open('mas-static-v4').then(function(cache) {
+    caches.open('mas-static-v5').then(function(cache) {
       // add the urls in urlsToCache to opened cache
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+*/
+
+// get cached assets
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
